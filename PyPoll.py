7a1e42de00b37f53f4f898f171ea6e219275d474
Caroline Styc <96352625+carolinestyc@users.noreply.github.com>
2022-01-23 20:05:19 -0800

    Adding the election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -42,12 +42,31 @@ with open(file_to_load) as election_data:
         #add a vote to the candidates count
         candidate_votes[candidate_name] += 1
 
+with open(file_to_save, "w") as txt_file:
+    # Print the final vote count to the terminal.
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+    # Save the final vote count to the text file.
+    txt_file.write(election_results)
+
     #determine the percentage of votes for each candidate by looping through the counts
     for candidate_name in candidate_votes:
         # Retrieve vote count of a candidate
         votes = candidate_votes[candidate_name]
         # Calculate the percentage of votes.
         vote_percentage = float(votes) / float(total_votes) * 100
+        candidate_results = (
+            f"{candidate_name}: {vote_percentage:.2f}% ({votes:,})\n")
+        
+        # Print each candidate's voter count and percentage to the terminal.
+        print(candidate_results)
+        #  Save the candidate results to our text file.
+        txt_file.write(candidate_results)
+
         #determine  winning vote count and candidate
         # determine if the votes are greater than the winning count
         if (votes > winning_count) and (vote_percentage > winning_percentage):
@@ -57,7 +76,7 @@ with open(file_to_load) as election_data:
             winning_percentage = vote_percentage
             # And, set the winning_candidate equal to the candidate's name.
             winning_candidate = candidate_name
-        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        #print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
     #print out winning candidate summary
     winning_candidate_summary = (
@@ -65,5 +84,7 @@ with open(file_to_load) as election_data:
         f"Winner: {winning_candidate}\n"
         f"Winning Vote Count: {winning_count:,}\n"
         f"Winning Percentage: {winning_percentage:.1f}%\n"
-        f"--------------------------/n")
+        f"--------------------------")
+    #print(winning_candidate_summary)
     print(winning_candidate_summary)
+    txt_file.write(winning_candidate_summary)
